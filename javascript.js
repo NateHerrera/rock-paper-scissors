058eeaadfaa99d46338a5bf413e3ab1ea95e7ce0
Nate <150736820+NateHerrera@users.noreply.github.com>
2024-07-01 17:40:21 -0500

    Finished the computer choice, rock, paper, scissors function.

--- javascript.js
+++ javascript.js
@@ -1,2 +1,18 @@
+function getComputerChoice(){
+    
+    let max = 3;
+    let randomValue = Math.floor(Math.random() * max);
+    
+    switch(randomValue){
+        case 0: 
+            return "rock";
+        case 1: 
+            return "paper";
+        case 2:
+            return "scissors";
+        default:
+            return "Invalid choice";
+    }
+}
 
-
+console.log(getComputerChoice());
